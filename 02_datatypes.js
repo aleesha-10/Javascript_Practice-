c0aefe6bf06cf5e237b8774345eb76bddfc5cf9c
aleesha <aleeshabukhare@gmail.com>
2025-01-11 20:02:41 +0500

    fifth commit

---
+++ 02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // all JS code will be used as the newer version
+
+// alert(3+3) // error bec we're working w node rn
+
+console.log(3 + 3); // 6
+
+let name = "aleesha" // string
+let age = 20  // number
+let isLoggedIn = false // bool
+
+
+
+// number => to power 53 ( range)
+// bigint ( for bigger numbers used in websites like facebook)
+// string => ""
+// null => standalone value 
+// undefined => intentionally null
+// symbol => unique 
+// object 
+
+
+
+console.log(typeof age) // number
+console.log(typeof undefined) // undefined 
+console.log(typeof null) // object
+
+
+
