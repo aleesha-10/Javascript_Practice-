377ae5ee3363c2903daec8fe276bd89cd91b22e4
aleesha <aleeshabukhare@gmail.com>
2025-01-12 19:51:04 +0500

    comparison

---
+++ 04_comparison.js
@@ -0,0 +1,17 @@
+
+console.log(2>3); // the answers will be in boolean values 
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2!=1);
+
+//console.log("2" > 1); // true , JS automatically converts it because theyre the same datatypes i.e numbers
+console.log("02" > 1);
+
+//console.log(null > 0); // false
+console.log( null == 0) // false
+//console.log(  null >= 0) // true
+
+console.log(undefined == 0) // false 
+
+ // === => strict check
+ console.log("2"===2); // wont convert it so theyre not equal
