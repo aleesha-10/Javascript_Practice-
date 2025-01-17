21972a0e63ff659efe17e5cfa6960994a58814c8
aleesha <aleeshabukhare@gmail.com>
2025-01-17 20:37:36 +0500

    while

---
+++ 20_while.js
@@ -0,0 +1,23 @@
+// While 
+let i = 0
+while (i<=2) {
+    //console.log("hello");
+    i++;
+}
+
+let myArray = ["A","B","C"]
+
+let arr = 0
+while (arr<=2) {
+   // console.log(`value is ${myArray[arr]}`);
+    arr++
+    
+}
+
+// +++++++ DO - While
+ let score = 1
+
+do {
+    console.log(`Score is : ${score}`)
+    score++
+} while (score<=5);
